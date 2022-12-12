99eec0045bc7640069802990d8537c72a08c6168
jannejki <jannejki@metropolia.fi>
2022-12-12 19:00:58 +0200

    measurement working

---
+++ project_greenHouse/src/modbusConfig.cpp
@@ -0,0 +1,39 @@
+/*
+ * modbusConfig.cpp
+ *
+ *  Created on: 4 Nov 2022
+ *      Author: Manish
+ */
+
+#include "chip.h"
+
+#include "modbusConfig.h"
+
+modbusConfig::modbusConfig():
+	// TODO Auto-generated constructor stub
+	node_hmp(HMP60_ID),
+	node_gmp(GMP252_ID),
+	temp_(&node_hmp, read_temp_r_ADD, true),
+	rh_(&node_hmp, read_rh_r_ADD, true),
+	co2_(&node_gmp, read_co2_r_ADD, true)
+	{
+		node_hmp.begin(TRANSMISSION_RATE);
+		node_gmp.begin(TRANSMISSION_RATE);
+	}
+
+
+int modbusConfig::get_temp(){
+	return temp_.read()/10;
+}
+
+int modbusConfig::get_rh(){
+    return rh_.read()/10;
+}
+
+int modbusConfig::get_co2(){
+   return (co2_.read()/10)*100;
+}
+
+modbusConfig::~modbusConfig() {
+	// TODO Auto-generated destructor stub
+}

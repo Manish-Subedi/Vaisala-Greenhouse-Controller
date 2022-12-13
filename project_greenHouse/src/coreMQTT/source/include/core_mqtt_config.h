4e09f5efc9811a85ac1fed4c89d4b64f19617ce2
jannejki <jannejki@metropolia.fi>
2022-12-13 13:38:18 +0200

    links

---
+++ project_greenHouse/src/coreMQTT/source/include/core_mqtt_config.h
@@ -0,0 +1,13 @@
+/*
+ * core_mqtt_config.h
+ *
+ *  Created on: 7.12.2021
+ *      Author: keijo
+ */
+
+#ifndef COREMQTT_SOURCE_INCLUDE_CORE_MQTT_CONFIG_H_
+#define COREMQTT_SOURCE_INCLUDE_CORE_MQTT_CONFIG_H_
+
+
+
+#endif /* COREMQTT_SOURCE_INCLUDE_CORE_MQTT_CONFIG_H_ */
